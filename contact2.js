08fd1737ecfa4e23c9028d9bc96a16244ab70e88
matthieukhoda <68857955+matthieukhoda@users.noreply.github.com>
2020-11-16 21:30:34 +0100

    Add files via upload

---
+++ contact2.js
@@ -0,0 +1,12 @@
+
+setTimeout(function(){
+    document.getElementById("container"). classList.add("hidden")},100)
+
+
+var valider=document.getElementById("mc-embedded-subscribe").click();
+
+
+
+
+
+
